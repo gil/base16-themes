42e78db00da2df58fee516f7ed78f6c007b3bfef
Andre Gil <contato@andregil.org>
2020-08-03 23:44:55 +0200

    After building, moving airline output to a plugin-like structure

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ pybase16 build
 # vim airline fix
 cd output/base16-vim-airline
 ../../templates/base16-vim-airline/fix-dashes.sh
+rm -rf autoload/airline
+mkdir -p autoload/airline
+mv themes autoload/airline/
