8982bdf33e59a9e61fa69f69b4daad34791b6dfd
Andre Gil <contato@andregil.org>
2020-08-03 23:25:39 +0200

    New files for airline, with corrected names

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+# update 
 git submodule update --init --recursive --rebase
 pip install pybase16-builder --upgrade
+
+# build
 pybase16 build
+
+# vim airline fix
+cd output/base16-vim-airline
+../../templates/base16-vim-airline/fix-dashes.sh
